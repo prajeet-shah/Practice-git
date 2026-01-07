77246c722f929989cf08d4f4a8f8f5b385ca1666
prajeet shah <prajeetshah93@gmail.com>
2026-01-07 12:57:29 +0530

    add multiply feature

--- add.py
+++ add.py
@@ -14,6 +14,12 @@ def subtract_two_numbers(a, b):
     return a - b
 
 
+def multiply_two_numbers(a, b):
+    """
+    Returns the product of two numbers.
+    """
+    return a * b
+
 
 
 
@@ -23,11 +29,14 @@ def main():
 
     total = add_two_numbers(num1, num2)
     difference = subtract_two_numbers(num1, num2)
+    product = multiply_two_numbers(num1, num2)
+
     
 
     print(f"Sum of {num1} and {num2}: {total}")
     print(f"Difference of {num1} and {num2}: {difference}")
-    
+    print(f"Product of {num1} and {num2}: {product}")
+
 
 
 if __name__ == "__main__":
